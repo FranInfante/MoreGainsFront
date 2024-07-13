4b4d0bc40fd2fdcca43fbf6fddd90d2c7b7aa339
Fran <infante.portero@gmail.com>
2024-07-13 22:44:08 +0200

    empty landing component

---
+++ src/app/modules/+home/components/landingcomponents/landingcomponents.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-landingcomponents',
+  standalone: true,
+  imports: [],
+  templateUrl: './landingcomponents.component.html',
+  styleUrl: './landingcomponents.component.css'
+})
+export class LandingcomponentsComponent {
+
+}
