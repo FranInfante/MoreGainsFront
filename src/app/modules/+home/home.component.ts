ab17bd2b47ff373c93a6516a37c226b8602a930e
Fran <infante.portero@gmail.com>
2024-07-16 09:58:53 +0200

    logo added first version of footer done

--- src/app/modules/+home/home.component.ts
+++ src/app/modules/+home/home.component.ts
@@ -1,13 +1,14 @@
 import { Component } from '@angular/core';
 import { HeroComponent } from './components/hero/hero.component';
 import { LandingcomponentsComponent } from "./components/landingcomponents/landingcomponents.component";
+import { FooterComponent } from "../../shared/components/footer/footer.component";
 
 @Component({
     selector: 'app-home',
     standalone: true,
     templateUrl: './home.component.html',
     styleUrl: './home.component.css',
-    imports: [HeroComponent, LandingcomponentsComponent]
+    imports: [HeroComponent, LandingcomponentsComponent, FooterComponent]
 })
 export class HomeComponent {
 
--- src/app/shared/components/constants.ts
+++ src/app/shared/components/constants.ts
@@ -1,5 +1,6 @@
 export const ASSET_URLS = {
   herovideo: 'landingpage/herovid.mp4',
   sample1: 'landingpage/sample1.mp4',
-  sample2: 'landingpage/sample2.mp4'
+  sample2: 'landingpage/sample2.mp4',
+  logo: 'logo.png'
 };
