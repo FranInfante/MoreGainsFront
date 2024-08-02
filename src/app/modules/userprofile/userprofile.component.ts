05154cf91e708d264cc46004f7d186e1a3126d74
Fran <infante.portero@gmail.com>
2024-08-02 21:27:50 +0200

    updated

--- src/app/modules/userprofile/userprofile.component.ts
+++ src/app/modules/userprofile/userprofile.component.ts
@@ -2,27 +2,29 @@ import { Component, ElementRef, OnDestroy, OnInit} from '@angular/core';
 import { FormControl, FormGroup, FormsModule, ReactiveFormsModule, Validators } from '@angular/forms';
 import { UserService } from '../../shared/service/user.service';
 import { User } from '../../shared/interfaces/users';
-import { NgIf } from '@angular/common';
+import { CommonModule, NgIf } from '@angular/common';
 import { SubscriptionLike } from 'rxjs';
 import { ASSET_URLS, LOCATIONS } from '../../shared/components/constants';
 import { Router } from '@angular/router';
+import { PrivacySetting } from '../../shared/interfaces/enums/EnumPrivacySetting';
 
 @Component({
   selector: 'app-user-profile',
   standalone: true,
-  imports: [FormsModule, ReactiveFormsModule, NgIf],
+  imports: [FormsModule, ReactiveFormsModule, NgIf, CommonModule],
   templateUrl: './userprofile.component.html',
   styleUrl: './userprofile.component.css'
 })
 export class UserProfileComponent implements OnInit, OnDestroy {
   userForm!: FormGroup;
-  userId: number = 1;
+  userId: number | null = null; 
   user!: User;
   isEditing: boolean = false;
   subscriptions: SubscriptionLike[] = [];
   userIcon = ASSET_URLS.genericlogo;
+  privacySettings = Object.values(PrivacySetting);
 
-  constructor(private userService: UserService, private router: Router) { }
+  constructor(private userService: UserService, private router: Router) {}
 
   ngOnInit() {
     this.gatherUserId();
@@ -31,58 +33,62 @@ export class UserProfileComponent implements OnInit, OnDestroy {
   }
 
   ngOnDestroy(): void {
-    this.subscriptions.forEach(
-      (subscription) => subscription.unsubscribe());
+    this.subscriptions.forEach(subscription => subscription.unsubscribe());
   }
 
   gatherUserId() {
     this.subscriptions.push(this.userService.getCurrentUser().subscribe(user => {
       if (user && user.id) {
-        this.userId = user?.id;
+        this.userId = user.id;
       }
     }));
   }
 
   initForm() {
     this.userForm = new FormGroup({
-      name: new FormControl({ value: '', disabled: true }, [Validators.required, Validators.minLength(3)]),
-      surname: new FormControl({ value: '', disabled: true }, [Validators.required, Validators.minLength(3)]),
       username: new FormControl({ value: '', disabled: true }, [Validators.required, Validators.minLength(5)]),
       password: new FormControl({ value: '', disabled: true }, [Validators.required, Validators.minLength(6)]),
-      email: new FormControl({ value: '', disabled: true }, [Validators.email]),
-      dni: new FormControl({ value: '', disabled: true }, [Validators.pattern(/^\d{8,9}[A-Za-z]?$/)]),
-      phone: new FormControl({ value: '', disabled: true }, [Validators.pattern(/^[0-9]{9}$/)]),
-      postcode: new FormControl({ value: '', disabled: true }, [Validators.pattern(/^[0-9]{5}$/)]),
-      address: new FormControl({ value: '', disabled: true }, [Validators.required]),
-      available: new FormControl({ value: true, disabled: true })
+      email: new FormControl({ value: '', disabled: true }, [Validators.required, Validators.email]),
+      bio: new FormControl({ value: '', disabled: true }),
+      privacySetting: new FormControl({ value: '', disabled: true }, [Validators.required]),
+      isAvailable: new FormControl({ value: true, disabled: true })
     });
   }
 
   loadUser() {
-    this.subscriptions.push(this.userService.getUserById(this.userId).subscribe({
-      next: (data) => {
-        this.user = data;
-        this.userForm.patchValue(data);
-      }
-    }));
+    if (this.userId !== null) {
+      this.subscriptions.push(
+        this.userService.getUserById(this.userId).subscribe({
+          next: (data) => {
+            this.user = data;
+            this.userForm.patchValue(data);
+          },
+          error: (err) => console.error('Error loading user:', err)
+        })
+      );
+    } else {
+      console.error('User ID is null. Cannot load user.');
+    }
   }
 
   updateUser() {
-    if (this.userForm.valid) {
-      this.subscriptions.push(this.userService.updateUser(this.userId, this.userForm.value).subscribe({
-        next: () => {
-          this.loadUser();
-          this.userService.fetchAndSetUser();
-          this.userForm.disable();
-          this.isEditing = false;
-        }
-      }));
+    if (this.userId !== null && this.userForm.valid) {
+      this.subscriptions.push(
+        this.userService.updateUser(this.userId, this.userForm.getRawValue()).subscribe({
+          next: () => {
+            this.loadUser();
+            this.userService.fetchAndSetUser();
+            this.userForm.disable();
+            this.isEditing = false;
+          }
+        })
+      );
     }
   }
 
   deactivateAccount() {
     this.userForm.enable();
-    this.userForm.get('available')!.setValue(false);
+    this.userForm.get('isAvailable')!.setValue(false);
     this.updateUser();
     this.userService.logout();
     this.router.navigate([LOCATIONS.menu]);
@@ -92,13 +98,14 @@ export class UserProfileComponent implements OnInit, OnDestroy {
     this.isEditing = !this.isEditing;
     if (this.isEditing) {
       this.userForm.enable();
+      this.userForm.get('isAvailable')!.disable();
     } else {
       this.userForm.disable();
     }
   }
 
   toggleCancel() {
-    if (this.isEditing){
+    if (this.isEditing) {
       this.toggleEdit();
       this.loadUser();
     }
