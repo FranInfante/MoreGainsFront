6f675c4fcb270c7013b293528abc104c029be264
Fran <infante.portero@gmail.com>
2024-08-05 16:09:44 +0200

    updated

---
+++ src/app/shared/components/back-to-menu/back-to-menu.component.ts
@@ -0,0 +1,20 @@
+import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+import { LOCATIONS } from '../constants';
+
+@Component({
+  selector: 'app-back-to-menu',
+  standalone: true,
+  imports: [],
+  templateUrl: './back-to-menu.component.html',
+  styleUrl: './back-to-menu.component.css'
+})
+export class BackToMenuComponent {
+
+  constructor(private router: Router) {}
+
+  goToMenu() {
+    this.router.navigate([LOCATIONS.menu]);
+  }
+
+}
