9e52d32627e8e2ff0e8e4898cd41042579f42c88
Fran <infante.portero@gmail.com>
2024-08-05 16:18:37 +0200

    back to menu navbar

--- src/app/shared/components/back-to-menu/back-to-menu.component.ts
+++ src/app/shared/components/back-to-menu/back-to-menu.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 import { Router } from '@angular/router';
 import { LOCATIONS } from '../constants';
 
@@ -10,6 +10,8 @@ import { LOCATIONS } from '../constants';
   styleUrl: './back-to-menu.component.css'
 })
 export class BackToMenuComponent {
+  
+  @Input() displayText: string = '';
 
   constructor(private router: Router) {}
 
