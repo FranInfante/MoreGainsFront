b81556805642ebab06c6c3af2857e17a059fdc58
Fran <infante.portero@gmail.com>
2024-08-15 17:25:54 +0200

    updated

--- src/app/modules/plans/components/workouts/workouts.component.ts
+++ src/app/modules/plans/components/workouts/workouts.component.ts
@@ -1,12 +1,15 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { Plan } from '../../../../shared/interfaces/plan';
+import { CommonModule } from '@angular/common';
+import { Workout } from '../../../../shared/interfaces/workout';
 
 @Component({
   selector: 'app-workouts',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './workouts.component.html',
   styleUrl: './workouts.component.css'
 })
 export class WorkoutsComponent {
-
+  @Input() workouts: Workout[] | null | undefined = null; 
 }
