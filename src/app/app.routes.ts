23c12f3c74e4f022acc08fffed07d6756e1c9e5b
Fran <infante.portero@gmail.com>
2024-09-04 10:46:26 +0200

    updated + name in folder

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -4,7 +4,7 @@ import { SignInComponent } from './modules/+login/sign-in/sign-in.component';
 import { RegisterComponent } from './modules/+login/register/register.component';
 import { MenuComponent } from './modules/+menu/menu.component';
 import { UserProfileComponent } from './modules/+userprofile/userprofile.component';
-import { PlansComponent } from './modules/plans/plans.component';
+import { PlansComponent } from './modules/+plans/plans.component';
 
 
 export const routes: Routes = [
