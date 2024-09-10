10f2406b507d8b7440d71a99a07400d566c99c65
Fran <infante.portero@gmail.com>
2024-09-10 13:46:56 +0200

    removed unused import

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,6 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { NgbdToastInline } from "./shared/components/toast/toast.component";
-import { AngularSvgIconModule } from 'angular-svg-icon';
 
 @Component({
   selector: 'app-root',
