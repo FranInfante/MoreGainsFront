ca5be62d241440aec082dd15aa60a60fa2c1ee42
Fran <infante.portero@gmail.com>
2024-09-13 01:37:46 +0200

    import

--- src/app/modules/+plans/plans.component.ts
+++ src/app/modules/+plans/plans.component.ts
@@ -10,13 +10,14 @@ import { WorkoutsComponent } from "./components/workouts/workouts.component";
 import { ASSET_URLS, TOAST_MSGS } from '../../shared/components/constants';
 import { ToastService } from '../../shared/service/toast.service';
 import { PlanHeaderComponent } from './components/plan-header/plan-header.component';
+import { DeletePlanModalComponent } from "./components/delete-plan-modal/delete-plan-modal.component";
 
 @Component({
   selector: 'app-plan-tabs',
   standalone: true,
   templateUrl: './plans.component.html',
   styleUrls: ['./plans.component.css'],
-  imports: [CommonModule, FormsModule, BackToMenuComponent, WorkoutsComponent, PlanHeaderComponent],
+  imports: [CommonModule, FormsModule, BackToMenuComponent, WorkoutsComponent, PlanHeaderComponent, DeletePlanModalComponent],
 })
 export class PlansComponent implements OnInit, AfterViewChecked {
   plans: Plan[] = [];
