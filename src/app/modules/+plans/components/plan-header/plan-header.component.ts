2fb3a452ad9c989a93902b83956ca0f46e000b77
Fran <infante.portero@gmail.com>
2024-09-16 13:31:18 +0200

    updated method maxlen

--- src/app/modules/+plans/components/plan-header/plan-header.component.ts
+++ src/app/modules/+plans/components/plan-header/plan-header.component.ts
@@ -14,11 +14,13 @@ export class PlanHeaderComponent {
   @Input() threeDotsIcon!: string;
   @Output() editModeToggle = new EventEmitter<void>();
   @Output() planDelete = new EventEmitter<void>();
-  @Output() planNameUpdated = new EventEmitter<Plan>(); 
+  @Output() planNameUpdated = new EventEmitter<Plan>();
 
-  constructor(private planService : PlanService){
+  maxLen = 20;
+  specialKeys = ['Backspace', 'Shift', 'Control', 'Alt', 'Delete'];
+  navigationalKeys = ['ArrowLeft', 'ArrowUp', 'ArrowRight', 'ArrowDown'];
 
-  }
+  constructor(private planService: PlanService) {}
 
   toggleEditMode(): void {
     this.editModeToggle.emit();
@@ -30,7 +32,7 @@ export class PlanHeaderComponent {
 
   updatePlanName(): void {
     const newName = (document.querySelector('h3') as HTMLElement)?.innerText.trim();
-  
+
     if (newName && newName !== this.activePlan.name) {
       this.planService.updatePlanName(this.activePlan.id, newName).subscribe({
         next: (updatedPlan) => {
@@ -42,5 +44,30 @@ export class PlanHeaderComponent {
       });
     }
   }
-  
+
+  onKeyDown(event: KeyboardEvent): boolean {
+    const input = event.target as HTMLElement;
+    const len = input.innerText.trim().length;
+    let hasSelection = false;
+    const selection = window.getSelection();
+    const key = event.key;
+
+    const isSpecial = this.specialKeys.includes(key);
+    const isNavigational = this.navigationalKeys.includes(key);
+
+    if (selection) {
+      hasSelection = !!selection.toString();
+    }
+
+    if (isSpecial || isNavigational) {
+      return true;
+    }
+
+    if (len >= this.maxLen && !hasSelection) {
+      event.preventDefault();
+      return false;
+    }
+
+    return true;
+  }
 }
